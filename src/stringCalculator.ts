07d1891335e67eb7d10d678cf4951dd78c17e3f5
Lakshmi Manasa Lingamallu <lakshmimanasa34@gmail.com>
2025-07-26 20:42:05 +0530

    feat: function ignore multi-character delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,17 +2,25 @@ export function addString(input: string): number {
     if (!input) return 0;
 
     let delimiterPattern = /[\n,]/; // Default delimiters: comma and newline
-    let numbersPart = input;
+    let numbersInput = input;
 
     // Check for custom delimiter
     if (input.startsWith("//")) {
-        const [delimiterLine, restOfInput] = input.split("\n");
-        const customDelimiter = delimiterLine.charAt(2); // safer than [2]
-        delimiterPattern = new RegExp(`[${customDelimiter}\n]`);
-        numbersPart = restOfInput;
+        const [delimiterPart, numbersPart] = input.split("\n");
+        numbersInput = numbersPart;
+
+        const delimiterMatch = delimiterPart.match(/\[(.+)\]/);
+        if (delimiterMatch) {
+            // Escape RegExp special characters inside delimiter
+            const escapedDelimiter = delimiterMatch[1].replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+            delimiterPattern = new RegExp(escapedDelimiter);
+        } else {
+            const customDelimiter = delimiterPart.charAt(2);
+            delimiterPattern = new RegExp(`[${customDelimiter}\n]`);
+        }
     }
 
-    const numbers = numbersPart
+    const numbers = numbersInput
         .split(delimiterPattern)
         .map(str => parseInt(str, 10));
 
