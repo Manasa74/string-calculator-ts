d37e36988ec3341c7f99d75bea057b2491ae4c49
Lakshmi Manasa Lingamallu <lakshmimanasa34@gmail.com>
2025-07-26 15:04:26 +0530

    feat: function returns single number as output

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,10 @@
 export function addString(input: string): number {
     // Given string is empty
     if (input === "") return 0;
+
+    // Given '5' as input returns 5.
+    if (!input.includes(",")) {
+        return parseInt(input);
+    }
     return -1;
 }
