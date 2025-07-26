f2ad4ed041f32e58e510f0ac9adb6acf1e6f1f4a
Lakshmi Manasa Lingamallu <lakshmimanasa34@gmail.com>
2025-07-26 15:19:37 +0530

    feat: function to support newline character as valid delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,7 @@
 export function addString(input: string): number {
   if (input === "") return 0;
 
-  const numbers = input.split(",").map(num => parseInt(num));
+  const numbers = input.split(/[\n,]/).map(num => parseInt(num));
   return numbers.reduce((sum, num) => sum + num, 0);
 }
 
