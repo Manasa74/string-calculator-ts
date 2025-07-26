a81af4e866efcc10d08a4a91b017bf3b23d8203b
Lakshmi Manasa Lingamallu <lakshmimanasa34@gmail.com>
2025-07-26 15:41:28 +0530

    feat: function supports custom delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,19 @@
 export function addString(input: string): number {
   if (input === "") return 0;
 
-  const numbers = input.split(/[\n,]/).map(num => parseInt(num));
+  let delimiterPattern = /[\n,]/; // default delimiters
+  let numbersPart = input;
+
+  if (input.startsWith("//")) {
+    const parts = input.split("\n");
+    const customDelimiter = parts[0][2];
+    delimiterPattern = new RegExp(`[${customDelimiter}\n]`);
+    numbersPart = parts[1];
+  }
+
+  const numbers = numbersPart
+    .split(delimiterPattern)
+    .map(num => parseInt(num));
+
   return numbers.reduce((sum, num) => sum + num, 0);
 }
-
